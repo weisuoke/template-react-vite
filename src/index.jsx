52044b114637e3510518a363918247dc0afd7be2
邬晓 <530264127@qq.com>
2021-12-26 17:04:34 +0800

    feat: 🎸 静态资源

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Logo from './assets/logo.png'
 
 const App = () => {
   // console.log(import.meta.env.VITE_APP_TEST)
   return (
-    <div>Vite React App {import.meta.env.VITE_APP_TEST}</div>
+    <>
+      <div>Vite React App {import.meta.env.VITE_APP_TEST}</div>
+      <div>
+        <img src={Logo} alt=""/>
+      </div>
+    </>
+
   )
 }
 
